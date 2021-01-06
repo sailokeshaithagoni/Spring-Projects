f0d9d58df3b6d533e1d8147ba7964a6756cccaa0
Sailokesh Aithagoni <sailokeshgoud@gmail.com>
2021-01-06 21:48:53 +0100

    Default Changelist

--- mongodb/src/main/java/com/example/mongodb/dataaccess/MongoDBClient.java
+++ mongodb/src/main/java/com/example/mongodb/dataaccess/MongoDBClient.java
@@ -11,8 +11,7 @@ public class MongoDBClient {
     public static String getDbConnection()
     {
         try {
-            MongoClientURI uri = new MongoClientURI(
-                    "mongodb+srv://sailokesh:rsTFxMAu5UHpOacL@cluster0.c8xds.mongodb.net/MongoDbDemo?retryWrites=true&w=majority");
+            MongoClientURI uri = new MongoClientURI("mongodb+srv://sailokesh:rsTFxMAu5UHpOacL@cluster0.c8xds.mongodb.net/MongoDbDemo?retryWrites=true&w=majority");
             MongoClient mongoClient = new MongoClient(uri);
             DB mongodb = (DB) mongoClient.getDatabase("MongoDbDemo");
             DBCollection dbCollection = mongodb.getCollection("DemoCollection");
